ea145f7703573bbb2afa5eb3923646961aa0790f
Karol Pisarski <karol.pisarski@outlook.com>
2023-12-15 07:44:05 +0100

    Changed script

--- Python deployment linux/qtdeploy.py
+++ Python deployment linux/qtdeploy.py
@@ -4,9 +4,9 @@ import os
 import re
 
 # Define paths
-app_binary = '/home/karol/Repositories/Modbus-Industrial-Diagnostic-Tool/build/build-Modbus-Industrial-Diagnostic-Tool-Desktop_Qt_6_5_3_GCC_64bit-Release/exe/YourAppName'  # Replace 'YourAppName' with your actual app name
+app_binary = '/home/karol/Repositories/Modbus-Industrial-Diagnostic-Tool/build/build-Modbus-Industrial-Diagnostic-Tool-Desktop_Qt_6_5_3_GCC_64bit-Release/exe/Modbus-Industrial-Diagnostic-Tool'
 qt_base_dir = '/home/karol/Qt/6.5.3/gcc_64'  # Adjust if needed
-deployment_dir = os.path.dirname(app_binary)  # Deployment directory is the same as the binary's directory
+deployment_dir = '/home/karol/Repositories/Modbus-Industrial-Diagnostic-Tool/build/build-Modbus-Industrial-Diagnostic-Tool-Desktop_Qt_6_5_3_GCC_64bit-Release/exe/'
 
 # Function to copy files and directories
 def copy(source, destination):
@@ -25,21 +25,25 @@ def parse_ldd_output(output):
             libs.append(match.group(1))
     return libs
 
-# Run ldd to find dependencies
-process = subprocess.run(['ldd', app_binary], stdout=subprocess.PIPE, text=True)
-dependencies = parse_ldd_output(process.stdout)
+try:
+    # Run ldd to find dependencies
+    process = subprocess.run(['ldd', app_binary], stdout=subprocess.PIPE, text=True)
+    dependencies = parse_ldd_output(process.stdout)
 
-# Copy dependencies (Qt libraries and other linked libs)
-for lib in dependencies:
-    if qt_base_dir in lib:  # Copy only Qt-related libraries
-        copy(lib, os.path.join(deployment_dir, os.path.basename(lib)))
+    # Copy dependencies (Qt libraries only)
+    for lib in dependencies:
+        if qt_base_dir in lib:
+            copy(lib, os.path.join(deployment_dir, os.path.basename(lib)))
 
-# Example: Copy Qt plugins (adjust based on your application's needs)
-qt_plugins_dir = os.path.join(qt_base_dir, 'plugins')
-for plugin_subdir in ['platforms', 'imageformats']:
-    source_dir = os.path.join(qt_plugins_dir, plugin_subdir)
-    if os.path.exists(source_dir):
-        copy(source_dir, os.path.join(deployment_dir, 'plugins', plugin_subdir))
+    # Example: Copy Qt plugins (adjust based on your application's needs)
+    qt_plugins_dir = os.path.join(qt_base_dir, 'plugins')
+    for plugin_subdir in ['platforms', 'imageformats']:
+        source_dir = os.path.join(qt_plugins_dir, plugin_subdir)
+        if os.path.exists(source_dir):
+            copy(source_dir, os.path.join(deployment_dir, 'plugins', plugin_subdir))
+
+except Exception as e:
+    print(f"Error occurred: {e}")
 
 print("Deployment completed. Files copied to:", deployment_dir)
 
